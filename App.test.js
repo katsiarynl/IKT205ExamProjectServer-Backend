e373e771dd96df091b154e5fcd7971e42eb2e834
Katsiaryna Labkouskaya <katsiaryn@uia.no>
2023-03-01 15:47:14 +0100

    feat: project startup

---
+++ App.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import App from "./App";
+
+//we decided to configure test by following the instructions on  https://docs.expo.dev/guides/testing-with-jest/
+//and //https://stackoverflow.com/questions/44294286/jest-test-dev-is-not-defined to configure jest
+//the following documentation: https://jestjs.io/docs/expect was also used to create a simple test
+
+describe("<App />", () => {
+  test("resolves to lemon", () => {
+    // make sure to add a return statement
+    return expect(true).toBe(true);
+  });
+});
