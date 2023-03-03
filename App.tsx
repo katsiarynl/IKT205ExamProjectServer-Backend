67d42cf03b74805ebdd9aabbe44b0b7455a7b105
katsiarynl <115145961+katsiarynl@users.noreply.github.com>
2023-03-03 18:58:49 +0100

    ci: test pipeline

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-// hey. test hello 
+// hey. test hello. test 5
 export default function App() {
   let a = "1";
   const b = "2";
