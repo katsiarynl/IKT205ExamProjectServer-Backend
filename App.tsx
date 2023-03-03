03c7e16c9246f90e3f0dd6d8344aefffc5f450c5
katsiarynl <115145961+katsiarynl@users.noreply.github.com>
2023-03-03 18:49:37 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-// hey
+// hey. test 
 export default function App() {
   let a = "1";
   const b = "2";
