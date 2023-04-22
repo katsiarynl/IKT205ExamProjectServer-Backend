6ab3b1a3348ba28a1a73494aa0a57867c0abc11a
Katsiaryna Labkouskaya <katsiaryn@uia.no>
2023-04-22 20:42:22 +0200

    fix: delete unused imports

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-  };
-};
-
-//  this is for testging a pull request. here
