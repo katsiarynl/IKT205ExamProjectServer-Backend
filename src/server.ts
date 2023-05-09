aab9699b66249c4e2f4f623ae6487f53ac0aeb7f
Katsiaryna Labkouskaya <katsiaryn@uia.no>
2023-05-09 20:35:33 +0200

    fix: place map items into try

--- src/server.ts
+++ src/server.ts
@@ -290,12 +290,11 @@ app.post("/nodemailer/:mail", async (req: Request, res: Response) => {
     },
   });
 
-  const items = req.body.data.map(
-    (item) =>
-      `Name: ${item.name}  Price: ${item.price} Quantity: ${item.cartQuantity}, \n`
-  );
-
   try {
+    const items = req.body.data.map(
+      (item) =>
+        `Name: ${item.name}  Price: ${item.price} Quantity: ${item.cartQuantity}, \n`
+    );
     const mailOptions = {
       from: "cook2goo@gmail.com",
       to: req.params.mail,
